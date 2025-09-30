7fda77de94c5d2e00e0e7bb04a350d5ea546759a
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-09-30 15:29:00 -0300

    aula 30/09 pt2

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app =  express();
+
+const PORT = 8080;
+
+const socialMedia = require('./routes/socialMedia.js');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
+
+app.use('/api', socialMedia);
+
+app.listen(PORT,() => {
+    console.log(`Servidor iniciado na porta ${PORT} : https://localhost:${PORT}`);
+});
--- src/models/socialMedia.js
+++ src/models/socialMedia.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const dataLogin = require('../controller/login.js');
+const postsGaleria = require('../controller/postsGaleria.js');
+
+router.get("/",(req, res) => {
+    res.send("Hello World");
+});
+
+//Rota para carregar imagens
+router.get('/imagens', postsGaleria.loadImages);
+
+//Rota para pegar a imagem específica do ID
+router.post('/getImage', postsGaleria.getImage);
+
+//Rota para comentar nos posts
+router.post('/comentar', postsGaleria.commentPosts);
+
+//Rota para mostrar todos os comentários
+router.get('/comentarios',postsGaleria.allComments);
+
+//Rota para dar like nos posts
+router.post('/likes', postsGaleria.likePosts);
+
+//Rota para logar na rede social
+router.post('/login', dataLogin.login);
+
+module.exports = router;
